dc99f77704141b7aa65ca9c45ca523d81da9bb85
Marcos <Markof1089@outlook.com.ar>
2024-07-03 22:45:56 -0300

    Se agrego el login de usuario, pero no esta finalizado para usuarios comunes

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express'); 
 require('dotenv').config();
+const jwt = require('jsonwebtoken'); 
 const productRutes = require('./routes/productRoutes'); 
-
+const userRutes = require('./routes/loginRoutes'); 
+const cookieParser = require('cookie-parser'); 
 
 const app = express(); 
 
@@ -10,8 +12,33 @@ app.use(express.static('public'));
 /* Permite tomar la información que lleve le formulario */
 app.use(express.json());
 app.use(express.urlencoded({extended: false})); 
+app.use(cookieParser()); 
 
+/* Iniciamos las rutas */
 app.use('/', productRutes);
+app.use('/', userRutes); 
+
+/* Funcion de verificación de token */
+
+const verificarToken = (req, res, next) =>{
+    const secretkey = process.env.SECRET_KEY;
+    const token = req.cookies.token;  
+    const decoded = jwt.verify(token, secretkey);
+    console.log(decoded);
+    //const token_limpio = token.split(' ')[1];  
+    if (!token){
+        return res.status(401).send('Acceso denegado'); 
+    }
+    try{
+        const decoded = jwt.verify(token, secretkey);
+        req.user = decoded; 
+        next();
+            
+    }catch (error){
+        return res.status(400).send('Token no proveído'); 
+    }
+}
+
 /* Servidor */
 app.get('/productos', (req, res) => {
     res.sendFile(__dirname + "/public/productos.html"); 
@@ -26,6 +53,15 @@ app.get('/update', (req, res) => {
 app.get('/delete', (req, res) => {
     res.sendFile(__dirname + "/public/delete.html");
     //res.send('Editar producto');  
+})
+
+/* Acceder a la ruta privada */
+app.get('/admin', verificarToken,  (req, res) =>{
+    if (req.user.role === 'admin') {
+        res.sendFile(__dirname + '/private/productos.html');
+    } else {
+        res.status(403).send('Acceso denegado');
+    }
 })
 
 // Error 404 
